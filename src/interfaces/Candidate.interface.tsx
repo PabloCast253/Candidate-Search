bb6b5b7d7c11ffdae2dc971f994cc4deb452dd78
unknown <pablocast253@gmail.com>
2025-01-28 15:56:09 -0800

    did the candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    name: string;
+    username: string;
+    location: string;
+    avatar: string;
+    email?: string;
+    html_url: string;
+    company?: string;
+  }
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import React from 'react';
 
 const CandidateSearch = () => {
   return <h1>CandidateSearch</h1>;
